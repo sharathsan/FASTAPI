12a8b2fed9b2ed0bd5c786454306b548d2fe005e
Sharath BS <sharath.bs.deepbyte@gmail.com>
2022-09-16 16:20:13 +0530

    second commit

--- main.py
+++ main.py
@@ -3,31 +3,29 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+
 app = FastAPI()
 
+
 class Post(BaseModel):
-    title:str
-    content:str
+    title: str
+    content: str
     published: bool = True
     rating: int = None
 
+
 @app.get("/")
 async def root():
-    return{"message": "Awesome!!!"}
+    return {"message": "Awesome!!!"}
+
 
 @app.get("/hosts")
 def get_posts():
-    return {"data":"your post has been liked"}
+    return {"data": "your post has been liked"}
+
 
 @app.post("/createposts")
-def create_posts(new_post:Post):
+def create_posts(new_post: Post):
     print(new_post)
     print(new_post.dict())
-    return{"data":"new_post"}
-
-
-
-
-
-
-
+    return {"data": "new_post"}
